e01677cf0a945fa6b09fe83358697e75288fc37d
senmos-cmd <senmos@gmail.com>
2025-10-16 08:27:40 +0000

    wewew

--- tcp_modbus_class.py
+++ tcp_modbus_class.py
@@ -4,7 +4,7 @@ def saberi_polja(polje1, polje2):
     """Sabere dve liste brojeva element po element."""
     return [a + b for a, b in zip(polje1, polje2)]
 
-polje1 = [1, 2, 3]
+polje1 = [33, 2, 3]
 polje2 = [4, 5, 6]
 rezultat = saberi_polja(polje1, polje2)
 print("Rezultat sabiranja polja:", rezultat)
